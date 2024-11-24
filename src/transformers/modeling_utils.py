faaeb1b2757f62acab02de02748239bb4b0f5883
Your Name <you@example.com>
2024-11-24 14:20:27 +0100

    py13

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -131,9 +131,10 @@ if is_accelerate_available():
         from accelerate.utils.modeling import get_state_dict_from_offload
 
 if is_safetensors_available():
-    from safetensors import safe_open
-    from safetensors.torch import load_file as safe_load_file
-    from safetensors.torch import save_file as safe_save_file
+    pass
+    #from safetensors import safe_open
+    #from safetensors.torch import load_file as safe_load_file
+    #from safetensors.torch import save_file as safe_save_file
 
 logger = logging.get_logger(__name__)
 
