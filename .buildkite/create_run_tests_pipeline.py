836aa6402833cfd4f9497ebd762b71459cc21053
ydshieh <ydshieh@users.noreply.github.com>
2025-02-21 17:41:13 +0100

    develop pipeline

--- .buildkite/create_run_tests_pipeline.py
+++ .buildkite/create_run_tests_pipeline.py
@@ -17,11 +17,6 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    with open(".buildkite/config2.yml") as fp:
-        data = fp.read()
-    with open(args.output_file, "w") as fp:
-        fp.write(data)
-
     config = dict()
     config["steps"] = []
     job = dict()
@@ -55,30 +50,9 @@ if __name__ == '__main__':
     ]
     config["steps"].append(job)
 
-
     folder = ".buildkite"
 
-    with open(os.path.join(folder, "generated_config.yml"), "w") as f:
+    print(args.output_file)
+    # with open(os.path.join(folder, args.output_file), "w") as f:
+    with open(args.output_file, "w") as f:
         f.write(yaml.dump(config, sort_keys=False, default_flow_style=False, width=float("inf")))
-
-    # with open(args.input_file) as fp:
-    #     data = fp.read().splitlines()
-    #
-    # n_splits = args.n_splits
-    #
-    # n_items = len(data)
-    # n_items_per_split = n_items // n_splits
-    # reminder = n_items % n_splits
-    # n_items = [n_items_per_split + int(idx < reminder) for idx in range(n_splits)]
-    # # print(n_items)
-    #
-    # final = {}
-    # current = 0
-    # for idx in range(n_splits):
-    #     end = current + n_items[idx]
-    #     final[idx] = data[current: end]
-    #     current = end
-    #
-    # import json
-    # with open(args.output_file, "w") as fp:
-    #     json.dump(final, fp, indent=4)
