df2fd6d7fd01f9bb91dfd8b01968febb2921020d
ydshieh <ydshieh@users.noreply.github.com>
2025-02-25 11:41:08 +0100

    develop pipeline

--- .buildkite/create_run_tests_pipeline.py
+++ .buildkite/create_run_tests_pipeline.py
@@ -6,17 +6,37 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, Optional
 
 
+COMMON_ENV_VARIABLES = {
+    "OMP_NUM_THREADS": 1,
+    "TRANSFORMERS_IS_CI": "true",
+    "PYTEST_TIMEOUT": 120,
+    "RUN_PIPELINE_TESTS": "false",
+}
+
+
 @dataclass
 class Job:
     name: str
+    additional_env: Dict[str, Any] = None
     docker_image: List[Dict[str, str]] = None
     install_steps: List[str] = None
+    marker: Optional[str] = None
 
     def __post_init__(self):
+        # Deal with defaults for mutable attributes.
+        if self.additional_env is None:
+            self.additional_env = {}
         if self.install_steps is None:
             self.install_steps = ["uv venv && uv pip install ."]
 
     def to_dict(self):
+        env = COMMON_ENV_VARIABLES.copy()
+        env.update(self.additional_env)
+
+        env = [f"{k}={v}" for k,v in env.items()]
+
+        # specific to BUILDKITE
+        env.extend(["BUILDKITE_PARALLEL_JOB", "BUILDKITE_BRANCH"])
 
         test_file = f"test_preparation/splitted_{self.job_name}_test_list.json"
         # `parallelism` should correspond to the test split file computed previously
@@ -31,15 +51,14 @@ class Job:
                     "image": self.docker_image,
                     "always-pull": "true",
                     "mount-buildkite-agent": "true",
-                    "environment": [
-                        "OMP_NUM_THREADS=1",
-                        "BUILDKITE_PARALLEL_JOB",
-                        "BUILDKITE_BRANCH",
-                    ]
+                    "environment": env
                 }
             }
         ]
         job["parallelism"] = parallelism
+
+        marker_cmd = f"-m '{self.marker}'" if self.marker is not None else ""
+
         job["commands"] = [
             "mkdir test_preparation",
             "buildkite-agent artifact download \"test_preparation/*\" test_preparation/ --step fetch_tests",
@@ -52,7 +71,7 @@ class Job:
             "dpkg-query --show --showformat='${Installed-Size}\t${Package}\n' | sort -rh | head -25 | sort -h | awk '{ package=$2; sub(\".*/\", "", package); printf(\"%.5f GB %s\n\", $1/1024/1024, package)}' || true",
             f"TEST_SPLITS=$(python -c 'import os; import json; fp = open(\"{test_file}\"); data = json.load(fp); fp.close(); test_splits = data[os.environ[\"BUILDKITE_PARALLEL_JOB\"]]; test_splits = \" \".join(test_splits); print(test_splits);')",
             "echo \"$$TEST_SPLITS\"",
-            "python -m pytest -n 8 -v $$TEST_SPLITS",
+            f"python -m pytest {marker_cmd} pytest -n 8 -v $$TEST_SPLITS",
         ]
 
         return job
@@ -66,13 +85,20 @@ class Job:
 torch_job = Job(
     "torch",
     docker_image="huggingface/transformers-torch-light",
-    # marker="not generate",
+    marker="not generate",
+)
+
+pipelines_torch_job = CircleCIJob(
+    "pipelines_torch",
+    additional_env={"RUN_PIPELINE_TESTS": "true"},
+    docker_image="huggingface/transformers-torch-light",
+    marker="is_pipeline_test",
 )
 
 generate_job = Job(
     "generate",
     docker_image="huggingface/transformers-torch-light",
-    # marker="generate",
+    marker="generate",
 )
 
 REGULAR_TESTS = [torch_job, generate_job]
